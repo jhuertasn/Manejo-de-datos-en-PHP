d68b3dbb43bf5972a0842c7ed566480e028f404e
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2023-05-11 10:07:11 -0500

    segundo commit

--- index.php
+++ index.php
@@ -6,3 +6,32 @@ comilla simple \' backslash \\ continuar con mas texto';
 */
 $name = 'italo';
 echo "Mi nombre es $name <br>";
+
+$courses = [
+    'backend' => [
+        'PHP'
+    ]
+];
+
+
+/*class User 
+{
+    public $name ='Jordan';
+}
+
+$user = new User;
+
+//echo "$user->name quiere aprender {$courses['backend']}";
+*/
+$teacher = 'Jordan';
+$Jordan = 'Profesor de PHP';
+
+echo "$teacher es ${$teacher} <br>";
+
+function getTeacher(){
+    return 'teacher'; 
+}
+
+$teacher = 'Jordan';
+
+echo "${getTeacher()} enseña PHP";
